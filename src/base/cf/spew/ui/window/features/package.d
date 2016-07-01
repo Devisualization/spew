efdbdea78a3a98db06c57c9397455eddb597ec98
U-AA\rikki <rikki@cattermole.co.nz>
2016-07-01 21:11:45 +1200

    Bit more work to get windowing abstraction setup

---
+++ src/base/cf/spew/ui/window/features/package.d
@@ -0,0 +1,5 @@
+﻿module cf.spew.ui.window.features;
+public import cf.spew.ui.window.features.icon;
+public import cf.spew.ui.window.features.menu;
+public import cf.spew.ui.window.features.screenshot;
+public import cf.spew.ui.window.features.cursor;
