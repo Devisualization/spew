00d61fef539e8be2f931438241aa1f3130088517
rikki cattermole <alphaglosined@gmail.com>
2017-04-16 22:59:44 +1200

    Overlay import bye bye

--- src/base/cf/spew/ui/context/features/vram.d
+++ src/base/cf/spew/ui/context/features/vram.d
@@ -8,7 +8,6 @@ module cf.spew.ui.context.features.vram;
 import cf.spew.ui.rendering;
 import cf.spew.ui.window.defs;
 import cf.spew.ui.context.defs;
-import cf.spew.ui.window.features.overlay : Have_OverlayWindow;
 import std.experimental.graphic.image : ImageStorage;
 import std.experimental.graphic.color : RGB8, RGBA8;
 
