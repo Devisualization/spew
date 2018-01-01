7c22de4f5fe718eaf68de689343de7fe58eb0d8d
rikki cattermole <alphaglosined@gmail.com>
2018-01-01 17:12:30 +1300

    Improving libuv event loop source, adding a timer to act as a timeout

--- src/base/cf/spew/event_loop/wells/libuv.d
+++ src/base/cf/spew/event_loop/wells/libuv.d
@@ -8,6 +8,7 @@ import cf.spew.events.defs;
 import std.experimental.allocator : ISharedAllocator, make;
 import devisualization.bindings.libuv.uv;
 import core.time;
+import core.atomic;
 
 uv_loop_t* getThreadLoop_UV() {
 	import devisualization.bindings.libuv.loader;
@@ -22,6 +23,9 @@ uv_loop_t* getThreadLoop_UV() {
 
 private {
 	uv_loop_t uvLoop;
+	uv_timer_t uvLoopTimeout;
+	bool uvLoopInitialized;
+
 	shared LibUVEventLoopSource uvLoopSource = new shared LibUVEventLoopSource;
 
 	static this() {
@@ -30,6 +34,9 @@ private {
 
 	static ~this() {
 		if (uvLoop.data !is null) {
+			if (uvLoopInitialized)
+				uv_close(cast(uv_handle_t*)&uvLoopTimeout, null);
+
 			uv_stop(&uvLoop);
 			uv_loop_close(&uvLoop);
 		}
@@ -62,21 +69,50 @@ final class LibUVEventLoopSource : EventLoopSource {
 
 final class LibUVEventLoopSourceRetrieve : EventLoopSourceRetriever {
 	import cf.spew.event_loop.known_implementations;
-	
+	import core.time : dur;
+
+	Duration timeout = dur!"seconds"(1);
+
 	bool nextEvent(ref Event event) shared {
 		// we can't return an event :(
 		// wrong event loop model
 		event.source = EventSources.LibUV;
 		// prevents any searching for a consumer (no event actually returned)
 		event.type.value = 0;
+
+		// empty event loop, important if no e.g. sockets are used.
+		if (uv_loop_alive(&uvLoop) == 0)
+			return false;
+		else if (!uvLoopInitialized) {
+			uvLoopInitialized = true;
+			uv_timer_init(&uvLoop, &uvLoopTimeout);
+		}
+
+		// setup a timer so we can find out if we have gone beyond what we are allowed.
+		ulong timeoutms = cast(ulong)atomicLoad(timeout).total!"msecs";
+		uvLoopTimeout.data = &uvLoop;
+		uv_timer_start(&uvLoopTimeout, &uvLoopTimerCB, timeoutms, 0);
+
 		// tells the manager if there are more events
-		uv_run(&uvLoop, uv_run_mode.UV_RUN_NOWAIT);
-		return false;
+		uv_run(&uvLoop, uv_run_mode.UV_RUN_ONCE);
+		uv_timer_stop(&uvLoopTimeout);
+
+		return uvLoopTimeout.data is null;
 	}
 	
 	void handledEvent(ref Event event) shared {}
 	void unhandledEvent(ref Event event) shared {}
 	void handledErrorEvent(ref Event event) shared {}
 	// unsupported, but that is ok, we don't block if we don't get an event!
-	void hintTimeout(Duration timeout) shared {}
+	void hintTimeout(Duration timeout) shared { this.timeout = timeout; }
+}
+
+extern(C) {
+	void uvLoopTimerCB(uv_timer_t* timer) {
+		if (timer.data !is null) {
+			auto loop = cast(uv_loop_t*)timer.data;
+			timer.data = null;
+			uv_stop(loop);
+		}
+	}
 }
