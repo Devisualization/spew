977a633c4c3ea0244b04db3fc5b09c55e27e76e5
rikki cattermole <pbdadmin@gmail.com>
2018-05-17 06:07:24 +1200

    How on earth did I forget this?

---
+++ src/base/cf/spew/events/x11.d
@@ -0,0 +1,31 @@
+/**
+ * Copyright: <a href="http://www.boost.org/LICENSE_1_0.txt">Boost License 1.0</a>.
+ * Authors: $(LINK2 http://cattermole.co.nz, Richard Andrew Cattermole)
+ */
+module cf.spew.events.x11;
+import cf.spew.events.defs;
+
+///
+enum X11_Events_Types {
+    ///
+    Prefix = EventType.from("|x"),
+    ///
+    Unknown = EventType.from("|xunknwn"),
+    ///
+    Expose = EventType.from("|xexpose"),
+    /// ConfigureNotify
+    NewSizeLocation = EventType.from("|xnewsl"),
+    ///
+    DestroyNotify = EventType.from("|xdstrw"),
+}
+
+///
+struct X11_Events {
+    ///
+    X11_Event_ConfigureNotify configureNotify;
+}
+
+struct X11_Event_ConfigureNotify {
+    int x, y;
+    int width, height;
+}
